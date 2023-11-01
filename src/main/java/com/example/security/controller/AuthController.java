3431b43bd165bdba6abcf4bd0f0407d98d8e179b
Asus <bpardaboyev2000@gmail.com>
2023-11-01 13:01:23 +0500

    commoit 1

---
+++ src/main/java/com/example/security/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/token")
+    public String token(){
+        return "TbbbnjjjbOKEN";
+    }
+}
---
+++ src/main/java/com/example/security/controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.example.security.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+//    @GetMapping("/")
+//    public String hi() {
+//        return "HELLO";
+//    }
+//
+//    @GetMapping("/admin")
+//    @PreAuthorize("hasRole('ADMIN')")
+//    public String admin() {
+//        return "admin page";
+//    }
+//
+//
+//    @PreAuthorize("hasRole('MANAGER')")
+//    @GetMapping("/manager")
+//    public String manager() {
+//        return "manager page";
+//    }
+//
+//    @PreAuthorize("hasAnyRole('MANAGER','MANAGER')")
+//    @GetMapping("/all")
+//    public String all() {
+//        return "all page";
+//    }
+//
+//    @PreAuthorize("isAuthenticated()")
+//    @GetMapping("/user")
+//    public String user() {
+//        return "user page";
+//    }
+}
