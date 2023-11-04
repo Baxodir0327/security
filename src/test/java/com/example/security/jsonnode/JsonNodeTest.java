79d543780532096d58073feece0c8941c87385df
Asus <bpardaboyev2000@gmail.com>
2023-11-04 10:01:22 +0500

    generator node class

--- src/test/java/com/example/security/jsonnode/JsonNodeTest.java
+++ src/test/java/com/example/security/jsonnode/JsonNodeTest.java
@@ -9,41 +9,41 @@ import java.util.Iterator;
 
 public class JsonNodeTest {
 
-//    @Test
-//    void simpleCodeTest() throws JsonProcessingException {
-//        String jsonstring = """
-//                              {
-//                  "id": 1,
-//                  "name": "Leanne Graham",
-//                  "username": "Bret",
-//                  "email": "Sincere@april.biz",
-//                  "address": {
-//                    "street": "Kulas Light",
-//                    "suite": "Apt. 556",
-//                    "city": "Gwenborough",
-//                    "zipcode": "92998-3874",
-//                    "geo": {
-//                      "lat": "-37.3159",
-//                      "lng": "81.1496"
-//                    }
-//                  },
-//                  "phone": "1-770-736-8031 x56442",
-//                  "website": "hildegard.org",
-//                  "company": {
-//                    "name": "Romaguera-Crona",
-//                    "catchPhrase": "Multi-layered client-server neural-net",
-//                    "bs": "harness real-time e-markets"
-//                  }
-//                  "languages":["Java","Python","C++"]
-//                }
-//                """;
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode rootNode = objectMapper.readTree(jsonstring);
-//        JsonNode username = rootNode.get("username");
-//        System.out.println(username.asText());
-//
-//        System.out.println(rootNode.at("/address/geo/lat").asDouble());
-//    }
+    @Test
+    void simpleCodeTest() throws JsonProcessingException {
+        String jsonstring = """
+                              {
+                  "id": 1,
+                  "name": "Leanne Graham",
+                  "username": "Bret",
+                  "email": "Sincere@april.biz",
+                  "address": {
+                    "street": "Kulas Light",
+                    "suite": "Apt. 556",
+                    "city": "Gwenborough",
+                    "zipcode": "92998-3874",
+                    "geo": {
+                      "lat": "-37.3159",
+                      "lng": "81.1496"
+                    }
+                  },
+                  "phone": "1-770-736-8031 x56442",
+                  "website": "hildegard.org",
+                  "company": {
+                    "name": "Romaguera-Crona",
+                    "catchPhrase": "Multi-layered client-server neural-net",
+                    "bs": "harness real-time e-markets"
+                  }
+                  "languages":["Java","Python","C++"]
+                }
+                """;
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonstring);
+        JsonNode username = rootNode.get("username");
+        System.out.println(username.asText());
+
+        System.out.println(rootNode.at("/address/geo/lat").asDouble());
+    }
 
 
     @Test
